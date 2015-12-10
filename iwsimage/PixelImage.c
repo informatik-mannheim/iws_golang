ae241b7fc46881faa79742beb735bb5582ee2cde
Maritn Starman <m.starman@live.com>
2015-12-10 16:43:04 +0100

    added main

---
+++ iwsimage/PixelImage.c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "_cgo_export.h"
+
+#define BMP_HEADER_SIZE 54
+int restHeaderSize;
+int c_reader(void* callback, char *src) {
+	FILE* f = fopen(src, "rb");
+
+	if (f == NULL) {
+		fclose(f);
+		return 1;
+	}
+
+	unsigned char info[BMP_HEADER_SIZE];
+	fread(info, sizeof(unsigned char), BMP_HEADER_SIZE, f); // read the BMP_HEADER_SIZE-byte header
+
+	// extract image height and width from header
+	int width = *(int*) &info[18];
+	int height = *(int*) &info[22];
+	int infoSize = *(int*) &info[2];
+	int row_padded = ((width * 3) + 3) & (~3);
+
+	
+	restHeaderSize = (infoSize - row_padded * height) - BMP_HEADER_SIZE;
+	if (restHeaderSize > 0) {
+		restHeaderSize = 68;
+		unsigned char restHeader[restHeaderSize];
+		fread(restHeader, sizeof(unsigned char), restHeaderSize, f);
+	}
+
+	int size = row_padded * height;
+
+	getPixelInfo(callback, width, height);
+
+	long sum = 0;
+	unsigned char dataRow[row_padded];
+	for (int yIdx = 0; yIdx < height; yIdx++) {
+		fread(dataRow, sizeof(unsigned char), row_padded, f);
+		for (int xIdx = 0; xIdx < width; xIdx++) {
+			int xIdxColor = xIdx * 3;
+			handelPixelData(callback, dataRow[xIdxColor],
+					dataRow[xIdxColor + 1], dataRow[xIdxColor + 2]);
+		}
+
+	}
+
+	fclose(f);
+	return 0;
+}
+
+int c_writeDataToImageFile(int *data, char *fileName, int width, int height) {
+	FILE *f;
+	
+	unsigned char bmpfileheader[14] = { 'B', 'M', 0, 0, 0, 0, 0, 0, 0, 0,
+	BMP_HEADER_SIZE + restHeaderSize, 0, 0, 0 };
+	unsigned char bmpinfoheader[40] = { 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+			0, 24, 0 };
+	unsigned char bmppad[3] = { 0, 0, 0 };
+
+	unsigned char *img = (unsigned char *) malloc(
+			(3 * width * height) * sizeof(unsigned char));
+
+	for (int j = 0; j < height * width * 3; ++j) {
+		img[j] = (unsigned char) data[j];
+	}
+
+	int fileSize = width * height * 3 + BMP_HEADER_SIZE + restHeaderSize;
+
+	bmpfileheader[2] = (unsigned char) (fileSize);
+	bmpfileheader[3] = (unsigned char) (fileSize >> 8);
+	bmpfileheader[4] = (unsigned char) (fileSize >> 16);
+	bmpfileheader[5] = (unsigned char) (fileSize >> 24);
+
+	bmpinfoheader[4] = (unsigned char) (width);
+	bmpinfoheader[5] = (unsigned char) (width >> 8);
+	bmpinfoheader[6] = (unsigned char) (width >> 16);
+	bmpinfoheader[7] = (unsigned char) (width >> 24);
+	bmpinfoheader[8] = (unsigned char) (height);
+	bmpinfoheader[9] = (unsigned char) (height >> 8);
+	bmpinfoheader[10] = (unsigned char) (height >> 16);
+	bmpinfoheader[11] = (unsigned char) (height >> 24);
+
+	f = fopen(fileName, "wb");
+
+	if (f == NULL) {
+		fclose(f);
+		return 1;
+	}
+
+	fwrite(bmpfileheader, 1, 14, f);
+	fwrite(bmpinfoheader, 1, 40, f);
+	
+	if(restHeaderSize > 0) {
+		unsigned char restHeader[restHeaderSize];
+		for(int i = 0; i < restHeaderSize; i++) {
+			restHeader[i] = 0;
+		}
+		
+		fwrite(restHeader, 1, restHeaderSize, f);
+	}
+
+	for (int i = 0; i < height; i++) {
+		fwrite(img + width * i * 3, 3, width, f);
+		fwrite(bmppad, 1, (4 - (width * 3) % 4) % 4, f);
+	}
+
+	free(img);
+	fclose(f);
+
+	return 0;
+}
---
+++ iwsimage/imageContainer.go
@@ -0,0 +1,106 @@
+package iwsimage
+
+import (
+	"math"
+)
+
+const (
+	maxWorker = 100
+)
+
+// ImageData is a struct containing all image handle PixelData PixelData
+// is an int array holding all image pixel info. The pixel data
+// is stored in following order: (i+0):r (i+1):g (i+2):b and  column first
+type ImageData struct {
+	PixelData  []int
+	Width      int
+	Height     int
+}
+
+// NewImageData returns new instance of ImageData
+func NewImageData() (nid *ImageData) {
+	nid = &ImageData{}
+	
+	return
+}
+
+// SetSize resets the image size
+func (d *ImageData) SetSize(width, height int) {
+	d.Width, d.Height = width, height
+	d.PixelData = make([]int, 0, height*width*3)
+}
+
+// Copy makes a deep copy of a ImageData
+func (d *ImageData) Copy() *ImageData {
+	newImgData := NewImageData()
+	newImgData.Width, newImgData.Height = d.Width, d.Height
+
+	newImgData.PixelData = make([]int, d.Height*d.Width*3)
+	for i, e := range d.PixelData {
+		newImgData.PixelData[i] = e
+	}
+
+	return newImgData
+}
+
+// AssembleTop  assembles a image on top of it self
+func (d *ImageData) AssembleTop(a *ImageData) {
+	var newHeight = d.Height + a.Height
+	var newWidth = int(math.Min(float64(d.Width), float64(a.Width)))
+	newPixelArray := make([]int, newWidth*newHeight*3)
+
+	for y := 0; y < a.Height*3; y += 3 {
+		for x := 0; x < newWidth*3; x++ {
+			newPixelArray[y*newWidth+x] = a.PixelData[y*a.Width+x]
+		}
+	}
+
+	startPoit := a.Height * newWidth * 3
+
+	for y := 0; y < d.Height*3; y += 3 {
+		for x := 0; x < newWidth*3; x++ {
+			newPixelArray[y*newWidth+startPoit+x] = d.PixelData[y*d.Width+x]
+		}
+	}
+
+	d.Width, d.Height = newWidth, newHeight
+	d.PixelData = newPixelArray
+}
+
+// AssembleLeft  assembles a image on the left of it self
+func (d *ImageData) AssembleLeft(a *ImageData) {
+	var newWidth = d.Width + a.Width
+	var newHeight = int(math.Min(float64(d.Height), float64(a.Height)))
+	newPixelArray := make([]int, newWidth*newHeight*3)
+
+	for y := 0; y < newHeight*3; y += 3 {
+		for x := 0; x < d.Width*3; x++ {
+			newPixelArray[y*newWidth+x] = d.PixelData[y*d.Width+x]
+		}
+		for x := 0; x < a.Width*3; x++ {
+			newPixelArray[y*newWidth+x+d.Width*3] = a.PixelData[y*a.Width+x]
+		}
+	}
+
+	d.Width, d.Height = newWidth, newHeight
+	d.PixelData = newPixelArray
+}
+
+func (d *ImageData) addPixel(r, g, b int) {
+	d.PixelData = append(d.PixelData, r, g, b)
+}
+
+// SaveFile saves the image to a given path
+func (d *ImageData) SaveFile(dest string) error {
+	return savefile(dest, d)
+}
+
+// LoadFile reads a file from disk
+func (d *ImageData) LoadFile(src string) error {
+	return loadFile(src, d)
+}
+
+// Filter runs a Filter at the image data. The filter gets past as parameter
+func (d *ImageData) Filter(filter func(*ImageData) error) error {
+	return filter(d)
+}
